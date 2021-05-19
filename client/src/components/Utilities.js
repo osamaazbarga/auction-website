8cacd87e4691d7e4e8e05f300a126909087ae509
Osama Azbarga <osamaazbarga@gmail.com>
2021-05-19 22:40:00 +0300

    update server

--- client/src/components/Utilities.js
+++ client/src/components/Utilities.js
@@ -10,34 +10,34 @@ import Api from './Api/MainAPI';
 
 
 export async function getCategoriesApi() {
-        const req=await Api.get('categories')
+        const req=await Api.get('api/categories')
     return req.data
 }
 
 export async function getProductsApi() {
-    const req=await Api.get('products')
+    const req=await Api.get('api/products')
     return req.data
 }
 
 export async function getAuctionsApi() {
-    const req=await Api.get('auctions')
+    const req=await Api.get('api/auctions')
     return req.data
 }
 
 export async function getCategoryByIDApi(id) {
-    const req=await Api.get(`categories/${id}`)
+    const req=await Api.get(`api/categories/${id}`)
     return req.data
 
 }
 
 export async function changeStatusProduct(id) {
-    const req=await Api.get(`products/updatestatus/${id}`)
+    const req=await Api.get(`api/products/updatestatus/${id}`)
     return req.data
 
 }
 
 export async function countsOfBids(id) {
-    const req=await Api.get(`auctions/byproduct/${id}`)
+    const req=await Api.get(`api/auctions/byproduct/${id}`)
     console.log(req);
     return req.data
 
